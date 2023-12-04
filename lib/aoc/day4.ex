b25e4ca37c49230c04a6579345ccd82bdc849109
Oliver Pile <oliver.pile443@gmail.com>
2023-12-04 18:58:04 +0000

    Day 4 part 2 variable names tweak

--- lib/aoc/day4.ex
+++ lib/aoc/day4.ex
@@ -27,23 +27,22 @@ defmodule Aoc.Day4 do
   end
 
   def part2() do
-    total_cards = %{}
     Aoc.get_file_lines("./input/day4.txt")
-    |> Enum.reduce(total_cards, fn line, acc ->
+    |> Enum.reduce(%{}, fn line, total_cards ->
       current_card = Regex.scan(~r/Card\s*(\d+)/, line, capture: :all_but_first) |> List.to_string() |> String.to_integer()
 
-      acc = Map.update(acc, current_card, 1, fn existing -> existing + 1 end)
+      total_cards = Map.update(total_cards, current_card, 1, fn existing -> existing + 1 end)
 
-      current_card_count = Map.get(acc, current_card, 1)
+      current_card_count = Map.get(total_cards, current_card, 1)
 
       matches = count_of_matches(line)
 
       if matches != 0 do
-        Enum.reduce(current_card+1..current_card+matches, acc, fn v, acc1 ->
-          Map.update(acc1, v, current_card_count, fn existing -> existing + current_card_count end)
+        Enum.reduce(current_card+1..current_card+matches, total_cards, fn v, acc ->
+          Map.update(acc, v, current_card_count, fn existing -> existing + current_card_count end)
         end)
       else
-        acc
+        total_cards
       end
     end)
     |> Map.values()
